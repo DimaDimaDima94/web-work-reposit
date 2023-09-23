d74caa2105725337c6f4ec74ab707e400a77cda1
DimaDimaDima94 <143801671+DimaDimaDima94@users.noreply.github.com>
2023-09-23 19:04:29 +0300

    commit-verstka

---
+++ index.php
@@ -0,0 +1,494 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel ="stylesheet" href="css/style.min.css">
+    <title>Gulp проект</title>
+
+    <style>
+        body {
+         background: #191919;
+                 
+        }
+    </style>
+
+</head>
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+
+<body>
+<!-- <div ><table align="center" border="0">
+        <tr>        
+        <td colspan="1"><img class="fon" src="img/Desktop_All.png" width="100%" border="10" alt="">
+        </tr>
+    </table></div> -->
+
+
+  <div class="sait">
+    <div class="heading">
+      <div class="name-sait">
+        <div class="text-name">ЗвукоЗапись</div>
+      </div>
+      <div class="link-sait">
+        <div >
+          <table>
+          <tr>
+    <th><a href="#"> Дом </a></th>
+    <th><a href="#"> Эпизоды </a></th>
+    <th><a href="#"> О нас </a></th>
+    <th><a href="#"> Контакты </a></th>
+   </tr>
+          <!-- <a href="#"> Дом </a><a href="#"> Эпизоды </a><a href="#"> О нас </a><a href="#"> Контакты </a></div> -->
+          </table>
+        <!-- <div class="link-sait_link-home">
+          <div class="link">Дом</div>
+        </div>
+        <div class="link-sait_link-episodes">
+          <div class="link">Эпизоды</div>
+        </div>
+        <div class="link-sait_link-about">
+          <div class="link">О нас</div>
+        </div>
+        <div class="link-sait_link-contact">
+          <div class="link">Контакты</div>
+        </div> -->
+      </div>
+    </div>
+    <div class="face-sait">
+      <div class="face-sait_foto">
+        <div class="foto-big"><img src="img/avtor.png"  border="0"></div>
+      </div>
+      <div class="face-sait_text">
+        <div class="text-big">Сделаем подкаст <br>следующего <br><b>уровня<b></div>
+      </div>
+      <div class="face-sait_icon_S">
+      <img src="img/icon_S_All/icon_1.png"><img src="img/icon_S_All/icon_2.png"><img src="img/icon_S_All/icon_3.png"><img src="img/icon_S_All/icon_4.png"><img src="img/icon_S_All/icon_5.png">
+
+        <!-- <div class="face-sait_icon_S_icon1">
+          <div class="icon"><img src="img/icon_S_All/icon_1.png"></div>
+        </div>
+        <div class="face-sait_icon_S_icon2">
+          <div class="icon">2</div>
+        </div>
+        <div class="face-sait_icon_S_icon3">
+          <div class="icon">3</div>
+        </div>
+        <div class="face-sait_icon_S_icon4">
+          <div class="icon">4</div>
+        </div>
+        <div class="face-sait_icon_S_icon5">
+          <div class="icon">5</div>
+        </div> -->
+      </div>
+    </div>
+    <div class="episodes">
+      <div class="episodes_heading-text">
+        <div class="text-big">Последние эпизоды</div>
+      </div>
+      <div class="episodes_button-view">
+        <div class="button-quadr"><button>Все эпизоды</button></div>
+      </div>
+      <div class="episodes_episode-1">
+        <div class="episode-1_foto">
+          <div class="foto-medium"><img src="img/foto-medium.png"  border="0"></div>
+        </div>
+        <div class="episode-1_tag">
+          <div class="tag-episode"><button>Звук</button></div>
+        </div>
+        <div class="episode-1_number">
+          <div class="number-episode">Эпизод 1</div>
+        </div>
+        <div class="episode-1_text-big">
+          <div class="text-big">Какую аудио<br>клавиатуру выбрать</div>
+        </div>
+        <div class="episode-1_text-small">
+          <div class="text-small">Один из стойких стереотипов в мире <br>аудио гласит, что для прослушивания музыки <br>нужно использовать стереосистему, а для сопровождения <br>кинофильмов – многоканальную систему <br>окружающего звучания. </div>
+        </div>
+        <div class="episode-1_button-view">
+          <div class="button-quadr"><button>Посмотреть детали</button></div>
+        </div>
+      </div>
+      <div class="episodes_episode-2">
+        <div class="episode-2_foto">
+          <div class="foto-medium"><img src="img/foto-medium.png"   border="0"></div>
+        </div>
+        <div class="episode-2_tag">
+          <div class="tag-episode"><button>Микрофон</button></div>
+        </div>
+        <div class="episode-2_number">
+          <div class="number-episode">Эпизод 2</div>
+        </div>
+        <div class="episode-2_text-big">
+          <div class="text-big">Предлагаем микрофон <br>следующего уровня</div>
+        </div>
+        <div class="episode-2_text-small">
+          <div class="text-small">Микрофон для ПК – это устройство, которое позволяет <br>
+          записывать звуковые сигналы с помощью компьютера. Оно может быть использовано <br>
+          для различных целей, включая голосовое общение в интернете, запись музыки, создание<br>
+          подкастов, аудио- и видеосъемки</div>
+        </div>
+        <div class="episode-2_button-view">
+          <div class="button-quadr"><button>Посмотреть детали</button></div>
+        </div>
+      </div>
+      <div class="episodes_episode-3">
+        <div class="episode-3_foto">
+          <div class="foto-medium"><img src="img/foto-medium.png"   border="0"></div>
+        </div>
+        <div class="episode-3_tag">
+          <div class="tag-episode"><button>Подставка</button></div>
+        </div>
+        <div class="episode-3_number">
+          <div class="number-episode">Эпизод 3</div>
+        </div>
+        <div class="episode-3_text-big">
+          <div class="text-big">Предлагаем микрофон <br>следующего уровня</div>
+        </div>
+        <div class="episode-3_text-small">
+          <div class="text-small">Микрофонные стойки - это специальные изделия, которые используются для закрепления микрофона на удобной высоте, в требуемом положении, под заданным углом</div>
+        </div>
+        <div class="episode-3_button-view">
+          <div class="button-quadr"><button>Посмотреть детали</button></div>
+        </div>
+      </div>
+    <div class="face-sait-2">
+      <div class="face-sait-2_foto">
+        <div class="foto-big"><img src="img/avtor.png"   border="0"></div>
+      </div>
+      <div class="face-sait-2_button-circle">
+        <div class="button-circle"><button>&#8594;</button></div>
+      </div>
+      <div class="face-sait-2_text-big">
+        <div class="text-big">Джейкоб Паулнер</div>
+      </div>
+      <div class="face-sait-2_text-small">
+        <div class="text-small">Джейкоб играет на большом кол-ве <br>
+        инструментов, профессионально <br>записывает звук, 
+        избавляется от лишних <br>
+        шумов, делает объемные и чистые биты</div>
+      </div>
+    </div>
+    <div class="email">
+    <div class="email_text-small">
+        <div class="text-small">Впиши свои данные</div>
+      </div>
+      <div class="email_text-big">
+        <div class="text-big">Оставь <br>свой отзыв</div>
+      </div>
+      <!-- <div class="email_text-small">
+        <div class="text-small">Впиши свои данные</div>
+      </div> -->
+      </form>
+      <form name="test" method="post" action="#">
+      <div class="email_box-name">
+        <div class="box-name"><input type="text" size="40" value="Имя"></div>
+      </div>
+      
+      <div class="email_box-email">
+        <div class="box-email"><input type="text" size="40" value="E-mail"></div>
+      </div>
+      <div class="email_button-view">
+        <div class="button-quadr"><button>Отправить</button></div>
+      </div>
+      </form>
+    </div>
+    <div class="box-comment">
+
+    <table border="0">
+   <tr>
+    <td><div class="box-comment_comment_1">
+        <div class="comment_1_stars">
+          <div class="stars">&#9733; &#9733; &#9733; &#9733; </div>
+        </div>
+        <div class="comment_1_text-medium">
+          <div class="text-medium">Я не могу рекомендовать контент</div>
+        </div>
+        <div class="comment_1_text-small">
+          <div class="text-small">Бетта Лэйси</div>
+        </div>
+      </div></td>
+    <td><div class="box-comment_comment_2">
+        <div class="comment_2_stars">
+          <div class="stars">&#9733; &#9733; &#9733; </div>
+        </div>
+        <div class="comment_2_text-medium">
+          <div class="text-medium">Тяжело воспринимать </div>
+        </div>
+        <div class="comment_2_text-small">
+          <div class="text-small">Джесика Кнау</div>
+        </div>
+      </div></td>
+    <td><div class="box-comment_comment_3">
+        <div class="comment_3_stars">
+          <div class="stars">&#9733; &#9733; </div>
+        </div>
+        <div class="comment_3_text-medium">
+          <div class="text-medium">Это не плохой бизнес</div>
+        </div>
+        <div class="comment_3_text-small">
+          <div class="text-small">Адам Драйвер</div>
+        </div>
+      </div></td>
+   </tr>
+   <tr>
+    <td><div class="box-comment_comment_4">
+        <div class="comment_4_stars">
+          <div class="stars">&#9733; &#9733; &#9733; &#9733; &#9733;</div>
+        </div>
+        <div class="comment_4_text-medium">
+          <div class="text-medium">Ох Ох дайте мне еще звука</div>
+        </div>
+        <div class="comment_4_text-small">
+          <div class="text-small">Скот Адамс</div>
+        </div>
+      </div></td>
+    <td><div class="box-comment_comment_5">
+        <div class="comment_5_stars">
+          <div class="stars">&#9733; &#9733; &#9733; &#9733; </div>
+        </div>
+        <div class="comment_5_text-medium">
+          <div class="text-medium">Хорошая школа музыки</div>
+        </div>
+        <div class="comment_5_text-small">
+          <div class="text-small">Маркус Браун</div>
+        </div>
+      </div></td>
+    <td><div class="box-comment_comment_6">
+        <div class="comment_6_stars">
+          <div class="stars">&#9733; &#9733; &#9733; </div>
+        </div>
+        <div class="comment_6_text-medium">
+          <div class="text-medium">Я буду слушать это</div>
+        </div>
+        <div class="comment_6_text-small">
+          <div class="text-small">Стивен Бласт</div>
+        </div>
+      </div></td>
+  </tr>
+    </table>
+
+
+      <!-- <div class="box-comment_comment_1">
+        <div class="comment_1_stars">
+          <div class="stars">&#9733; &#9733; &#9733; &#9733; </div>
+        </div>
+        <div class="comment_1_text-medium">
+          <div class="text-medium">Я не могу рекомендовать контент</div>
+        </div>
+        <div class="comment_1_text-small">
+          <div class="text-small">Бетта Лэйси</div>
+        </div>
+      </div> -->
+      <!-- <div class="box-comment_comment_2">
+        <div class="comment_2_stars">
+          <div class="stars">&#9733; &#9733; &#9733; </div>
+        </div>
+        <div class="comment_2_text-medium">
+          <div class="text-medium">Тяжело воспринимать </div>
+        </div>
+        <div class="comment_2_text-small">
+          <div class="text-small">Джесика Кнау</div>
+        </div>
+      </div> -->
+      <!-- <div class="box-comment_comment_3">
+        <div class="comment_3_stars">
+          <div class="stars">&#9733; &#9733; </div>
+        </div>
+        <div class="comment_3_text-medium">
+          <div class="text-medium">Это не плохой бизнес</div>
+        </div>
+        <div class="comment_3_text-small">
+          <div class="text-small">Адам Драйвер</div>
+        </div>
+      </div> -->
+      <!-- <div class="box-comment_comment_4">
+        <div class="comment_4_stars">
+          <div class="stars">&#9733; &#9733; &#9733; &#9733; &#9733;</div>
+        </div>
+        <div class="comment_4_text-medium">
+          <div class="text-medium">Ох Ох дайте мне еще звука</div>
+        </div>
+        <div class="comment_4_text-small">
+          <div class="text-small">Скот Адамс</div>
+        </div>
+      </div> -->
+      <!-- <div class="box-comment_comment_5">
+        <div class="comment_5_stars">
+          <div class="stars">&#9733; &#9733; &#9733; &#9733; </div>
+        </div>
+        <div class="comment_5_text-medium">
+          <div class="text-medium">Хорошая школа музыки</div>
+        </div>
+        <div class="comment_5_text-small">
+          <div class="text-small">Маркус Браун</div>
+        </div>
+      </div> -->
+      <!-- <div class="box-comment_comment_6">
+        <div class="comment_6_stars">
+          <div class="stars">&#9733; &#9733; &#9733; </div>
+        </div>
+        <div class="comment_6_text-medium">
+          <div class="text-medium">Я буду слушать это</div>
+        </div>
+        <div class="comment_6_text-small">
+          <div class="text-small">Стивен Бласт</div>
+        </div>
+      </div> -->
+    </div>
+
+
+
+
+
+    <div class="footer_">
+
+    <table id="tb" border="0">
+   <tr>
+   <td>
+        <div class="text-name">ЗвукоЗапись</div>
+      </td>
+    <td><div class="footer_link-sait_link-home">
+          <div class="link">Дом</div>
+        </div></td>
+    <td><div class="footer_link-sait_plus_instruction">
+          <div class="link">Инструкция</div>
+        </div></td>
+    <td><div class="iconsS"><img width="18px" src="img/icon_S_All/icon_1.png"><img width="18px" src="img/icon_S_All/icon_2.png"><img width="18px" src="img/icon_S_All/icon_3.png"><img width="18px" src="img/icon_S_All/icon_4.png"><img width="18px" src="img/icon_S_All/icon_5.png"></div>
+</td>
+   </tr>
+   <tr>
+   <div class="footer_link-sait">
+    <td></td>
+    <td><div class="footer_link-sait_link-episodes">
+          <div class="link">Эпизоды</div>
+        </div></td>
+    <td><div class="footer_link-sait_plus_change">
+          <div class="link">Выбор</div>
+        </div></td>
+    <td></td>
+      </div>
+  </tr>
+  <tr>
+    <td></td>
+    <td><div class="footer_link-sait_link-about">
+          <div class="link">О нас</div>
+        </div></td>
+    <td><div class="footer_link-sait_plus_credit">
+          <div class="link">Кредит</div>
+        </div></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td></td>
+    <td><div class="footer_link-sait_link-contact">
+          <div class="link">Контакты</div>
+        </div></td>
+    <td><div class="footer_link-sait_plus_powered">
+          <div class="link">Сила</div>
+        </div></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td><div class="footer_link-sait_plus_licenses">
+          <div class="link">Лицензия</div>
+        </div></td>
+    <td></td>
+  </tr>
+  <tr>
+    <td></td>
+    <td></td>
+    <td><div class="footer_link-sait_plus_style">
+          <div class="link">Стиль</div>
+        </div></td>
+    <td></td>
+  </tr>
+      </table>
+
+
+
+      <!-- <div class="footer_name-sait">
+        <div class="text-name">ЗвукоЗапись</div>
+      </div> -->
+      <!-- <div class="footer_link-sait">
+        <div class="footer_link-sait_link-home">
+          <div class="link"> </div>
+        </div>
+        <div class="footer_link-sait_link-episodes">
+          <div class="link"></div>
+        </div>
+        <div class="footer_link-sait_link-about">
+          <div class="link"></div>
+        </div>
+        <div class="footer_link-sait_link-contact">
+          <div class="link"></div>
+        </div>
+      </div> -->
+      <div class="footer_link-sait_plus">
+        <div class="footer_link-sait_plus_style">
+          <div class="link"></div>
+        </div>
+        <div class="footer_link-sait_plus_instruction">
+          <div class="link"></div>
+        </div>
+        <div class="footer_link-sait_plus_change">
+          <div class="link"></div>
+        </div>
+        <div class="footer_link-sait_plus_credit">
+          <div class="link"></div>
+        </div>
+        <div class="footer_link-sait_plus_powered">
+          <div class="link"></div>
+        </div>
+        <div class="footer_link-sait_plus_licenses">
+          <div class="link"></div>
+        </div>
+      </div>
+      <div class="footer_icon_S">
+        <div class="footer_icon_S_icon1">
+          <div class="icon"></div>
+        </div>
+        <div class="footer_icon_S_icon2">
+          <div class="icon"></div>
+        </div>
+        <div class="footer_icon_S_icon3">
+          <div class="icon"></div>
+        </div>
+        <div class="footer_icon_S_icon4">
+          <div class="icon"></div>
+        </div>
+        <div class="footer_icon_S_icon5">
+          <div class="icon"></div>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  
+
+
+</body>
+
+    
+    
+    
+    
+    
+    
+    
+    
+
+
+</html>
